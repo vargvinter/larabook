f49661165aa9b00268e70b73d159623cf9b742c4
vargvinter <wachelka.rafal@gmail.com>
2014-07-25 13:02:14 +0000

    new commit

---
+++ Gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var mincss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css', function() {
+	gulp.src('app/assets/sass/main.scss')
+		.pipe(sass({style: 'compressed'}))
+		.pipe(mincss())
+		.pipe(autoprefixer('last 10 version'))
+		.pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/assets/sass/**/*.scss', ['css']);
+});
+
+gulp.task('default', ['watch']);
--- app/views/layouts/default.blade.php
+++ app/views/layouts/default.blade.php
@@ -4,15 +4,19 @@
 	<meta charset="UTF-8">
 	<title>Larabook</title>
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-	
+
+	@include('layouts.partials.nav')
+
 	<div class="container">
 
 		@yield('content')
 		
 	</div>
 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </body>
 </html>
