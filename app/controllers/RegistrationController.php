18cb40fa15e64a7b9b6f8dc03409fc935cb84b55
vargvinter <vagrant@homestead>
2014-07-26 19:25:53 +0000

    Registration Validation

--- app/controllers/RegistrationController.php
+++ app/controllers/RegistrationController.php
@@ -1,7 +1,15 @@
 <?php
 
+use Larabook\Forms\RegistrationForm;
+
 class RegistrationController extends \BaseController {
 
+	private $registrationForm;
+
+	public function __construct(RegistrationForm $registrationForm)
+	{
+		$this->registrationForm = $registrationForm;
+	}
 
 	/**
 	 * Show the form for creating a new resource.
@@ -22,6 +30,7 @@ class RegistrationController extends \BaseController {
 	 */
 	public function store()
 	{
+		$this->registrationForm->validate(Input::all());
 		$user = User::create(
 			Input::only('username', 'email', 'password')
 		);
